81c853dbb0a104b5d3afcc078068ad556d653807
Naveen <naveen3992@gmail.com>
2018-07-20 16:23:54 +0530

    refactored the code

--- index.py
+++ index.py
@@ -4,9 +4,22 @@ import imagehash
 import argparse
 import shelve
 import glob
+import pdb 
+
+def arguementinitializer():
+    # construct the argument parse and parse the arguments
+    #pdb.set_trace()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dataset", required = True, help = "path to input dataset of images")
+    ap.add_argument("-s", "--shelve", required = True, help = "output shelve database")
+    ap.add_argument("-q", "--query", required = False, help = "path to the query image")
+    ap.add_argument("-f","--fname",required = False, help="name of the function to be called")
+    args = vars(ap.parse_args())
+    return args
 
 # open the shelve database
-def create_indicies(imageth,databasepath):
+def create_indices(imageth,databasepath):
+    #pdb.set_trace()
     db = shelve.open(databasepath, writeback = True)
     # loop over the image dataset
     for imagePath in glob.glob(imageth + "/*.jpg"):
@@ -23,11 +36,29 @@ def create_indicies(imageth,databasepath):
     # close the shelf database
     db.close()
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required = True,
-                help = "path to input dataset of images")
-ap.add_argument("-s", "--shelve", required = True,
-                help = "output shelve database")
-args = vars(ap.parse_args())
-create_indicies(args["dataset"], args["shelve"])
+
+def findDuplicateImage(imageFolder, databasefolder, imagepath):
+    # open the shelve database
+    db = shelve.open(databasefolder)
+    # load the query image, compute the difference image hash, and
+    # and grab the images from the database that have the same hash
+    # value
+    query = Image.open(imagepath)
+    h = str(imagehash.dhash(query))
+    filenames = db[h]
+    print "Found %d images" % (len(filenames))
+    # loop over the images
+    for filename in filenames:
+        image = Image.open(imageFolder + "/" + filename)
+        image.show()
+    # close the shelve database
+    db.close()
+
+
+
+
+args = arguementinitializer()
+if args["fname"] == "create_indices" :
+    create_indices(args["dataset"], args["shelve"])
+elif args["fname"] == "findDuplicateImage" : 
+    findDuplicateImage(args["dataset"],args["shelve"], args["query"])
